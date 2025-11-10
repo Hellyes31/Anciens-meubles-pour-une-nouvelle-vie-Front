045427d4484e020b76aba5e0d008f1071598cbb6
Hellyes31 <elies.auguet@gmail.com>
2025-11-10 15:49:56 +0100

    update:css

--- adawardrobe/app/login/page.tsx
+++ adawardrobe/app/login/page.tsx
@@ -31,7 +31,7 @@ export default function LoginPage(){
         }
     };
 return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100  text-black">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-black">
       <form
         onSubmit={handleSubmit}
         className="bg-white p-8 rounded shadow-md w-96"
