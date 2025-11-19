0d1c760ad2587e3294461e42377817dd22adc3b5
Hellyes31 <elies.auguet@gmail.com>
2025-11-19 10:18:24 +0100

    updated: page admin

--- adawardrobe/app/admin/page.tsx
+++ adawardrobe/app/admin/page.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
-export default function admin() {
-  const [login, setLogin] = useState("");
+export default function AdminPage() {
+  const router = useRouter();
+  const [allowed, setAllowed] = useState(false);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
+    if (!token) {
+      router.push("/login");
+      return;
+    }
 
+    try {
+      const decoded: any = jwtDecode(token);
 
-    return(
-        <p>Au revoir</p>
-    )
+      if (decoded.role !== "ROLE_ADMIN") {
+        router.push("/unauthorized");
+        return;
+      }
+
+      setAllowed(true);
+    } catch (err) {
+      router.push("/login");
+    }
+  }, []);
+
+  if (!allowed) return <p>Chargement...</p>;
+
+  return (
+    <div>
+      <h1>Bienvenue sur la page admin, {}</h1>
+    </div>
+  );
 }
+
